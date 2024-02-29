91aa7ad4532716fdd8adc2cd3ecf64e7448f2b7a
natosafix <artemburdinnn@gmail.com>
2024-02-29 21:34:50 +0500

    docker and docker-compose complete

--- src/main/java/org/example/mapper/MessageMapper.java
+++ src/main/java/org/example/mapper/MessageMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface MessageMapper {
 
     MessageDto messageToMessageDto(Message message);
